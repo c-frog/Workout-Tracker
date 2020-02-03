e89e751725eb40f26de597c4648b248af2d49695
Clyde Plasencia <cquiatt@gmail.com>
2020-02-02 19:31:22 -0700

    addition of get routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,44 +1,43 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 const path = require("path");
+const db = require("../models");
 
 router.post("/api/workouts", ({ body }, res)  => {
-   Workout.create(body)
-   .then(dbWorkout => {
-      res.json(dbWorkout);
-   })
-   .catch(err => {
-      res.status(400).json(err)
+   db.Workout.create({})
+      .then(dbWorkout => {
+         res.json(dbWorkout);
+      })
+      .catch(err=>{
+         res.status(400).json(err);
+      })
    });
-});
 
 router.put('/api/workouts/:id', (req, res) => {
-   const {id: _id} = req.params;
-   const {body} = req.body;
+   const id = req.params.id;
+   const body = req.body;
 
-   const newWorkout = {
-      _id,
-      body
-   };
+   const newWorkout = { }
 
    Workout.findByIdAndUpdate(
-      _id, 
-      newWorkout,
+      id,
+      {$push:{ exercises: body }},
       (err, updatedWorkout) => {
          if (err) {
             res.json({
                newWorkout,
                success: false,
-               msg: 'Failed to update Workout'
-            }); 
+               msg: "Failed to update workout"
+            })
          } else {
-            res.json({newWorkout,
+            res.json({newWorkout, 
                success: true,
-               msg: 'Workout added'
-            });
-         };
-      });
-   });
+               msg: "Workout added"
+            })
+         }
+      }
+   )
+});
 
 router.get("/api/workouts", (req, res) => {
    Workout.find({})
@@ -61,6 +60,16 @@ router.get("/api/workouts/range", (req, res) => {
             res.status(400).json(err)
          })
 });
+
+router.get("api/workouts/:id", (req, res) => {
+   Workout.findById(req.params.id)
+      .then(dbWorkout => {
+         res.json(dbWorkout);
+      })
+      .catch(err => {
+         res.status(400).json(err)
+      })
+})
 
 // -------------- HTML ROUTES --------------------------------
 
